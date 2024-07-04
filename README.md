7639bc2dfa417bd06ccb33008c85f52afbec71b0
Abdelrahman <abdo2652149@gmail.com>
2024-07-04 08:47:57 +0300

    Charity System

---
+++ README.md
@@ -0,0 +1,58 @@
+# Charity Website
+
+## Overview
+Welcome to our Charity System web application! This system is designed to facilitate the management of charity cases. Admins can input various cases for inspection, and inspectors can review each case and provide a report on whether it merits assistance or not.
+
+See demo here: [link](https://github.com/mohamedhassan218/charity-website/blob/main/SDProject/Demo.gif)
+
+
+## Technologies Used
+- .NET Framework
+- SQL Server
+- Entity Framework
+
+## Setup Instructions
+To set up and run the Charity System web application on your local machine, please follow these steps:
+
+1. Clone the repository to your local machine:
+
+   ```
+   git@github.com:mohamedhassan218/charity-website.git
+   ```
+
+2. Open the project in your preferred IDE or text editor.
+
+3. Make sure you have the necessary dependencies installed. You might need to install:
+   - .NET Framework SDK
+   - Entity Framework
+
+4. Put your connection string in the appsettings.json file.
+
+5. Build the project:
+
+   ```
+   dotnet build
+   ```
+
+6. Run the project:
+
+   ```
+   dotnet run
+   ```
+
+
+## Usage
+- **Admin Features:**
+  - Log in as an admin user.
+  - Insert cases for inspection.
+  - Manage inspectors and their assignments.
+  - Register new admins.
+
+- **Inspector Features:**
+  - Log in as an inspector user.
+  - Review cases assigned to them.
+  - Provide reports on each case indicating whether it merits assistance or not.
+
+
+## Contributing
+We welcome contributions from the community. If you find any bugs or have suggestions for improvements, please feel free to open an issue or submit a pull request.
---
+++ SDProject/SDProject/Controllers/AdminController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SDProject.Data;
+using SDProject.Models;
+namespace SDProject.Controllers.Inspector
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHostEnvironment _environment;
+        public AdminController(ApplicationDbContext context, IHostEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return RedirectToAction("Data");
+        }
+        // Admin Home page.
+        public IActionResult Data() 
+        {
+            return View();
+        }
+        // Create Inspector Account.
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]  // to more security
+        public async Task<IActionResult> Create(CaseInspector inspector, IFormFile img_file)
+        {
+            // to create Images folder in the project Path.
+            string path = "wwwroot/images/"; // wwwroot/Img/
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            if (img_file != null)
+            {
+                path = Path.Combine(path, img_file.FileName); // for exmple : /Img/Photoname.png
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await img_file.CopyToAsync(stream);
+                    ViewBag.Message = string.Format("<b>{0}</b> uploaded.</br>", img_file.FileName.ToString());
+                }
+            }
+            return View();
+        }
+
+        // Add New Case.
+        [HttpGet]
+        public IActionResult Information()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Information(Case c)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(c);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Admin");
+            }
+            return View(c);
+        }
+        public IActionResult Inspectors()
+        {
+            var inspectors = _context.Inspectors;
+            return View(inspectors);
+        }
+        public IActionResult Reports()
+        {
+            var reports = _context.Reports.ToList();
+            return View(reports);
+        }
+    }
+}
